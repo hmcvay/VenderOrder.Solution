53fafdab11128937d4607a721171f850ca2d2e6c
hmcvay <hayleymcvay8@gmail.com>
2022-03-04 14:07:54 -0800

    Add index view to vendors controller where all vendors will display.

--- VendorOrder/Controllers/VendorController.cs
+++ VendorOrder/Controllers/VendorController.cs
@@ -1,13 +1,18 @@
 using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
-using VendorOrder;
+using VendorOrder.Models;
 
 namespace VendorOrder.Controllers
 {
   public class VendorController : Controller
   {
-
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
 
   }
 }
