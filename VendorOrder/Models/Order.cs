f6e98b2144b5e3a63ab44436831046aebb95e348
hmcvay <hayleymcvay8@gmail.com>
2022-03-04 12:25:26 -0800

    LUNCH

--- VendorOrder/Models/Order.cs
+++ VendorOrder/Models/Order.cs
@@ -6,12 +6,9 @@ namespace VendorOrder.Models
   {
     public string Title { get; set; }
     public string Description { get; set; }
-
     public int Price { get; set; }
-
     public int Date { get; set; }
     public int Id { get; }
-
     private static List<Order> _instances = new List<Order>{};
 
     public Order(string title, string description, int price, int date){
--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+using System.Collections.Generic;
+using System;
 using System.Collections.Generic;
 
 namespace VendorOrder.Models
