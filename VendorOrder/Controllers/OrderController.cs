6bfccdb5399ea292ae087345f8e76c799f50aa52
hmcvay <hayleymcvay8@gmail.com>
2022-03-04 14:46:10 -0800

    Add form to add orders for specific vendors, rename view from item - from to do list - to order.

--- VendorOrder/Controllers/OrderController.cs
+++ VendorOrder/Controllers/OrderController.cs
@@ -6,7 +6,12 @@ namespace VendorOrder.Controllers
 {
   public class OrderController : Controller
   {
-
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+       Vendor vendor = Vendor.Find(vendorId);
+       return View(vendor);
+    }
 
   }
 }
